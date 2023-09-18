fa1dc5c47cf3d544448b17d7218babdb7143a304
Alexandre Retamero <alexandreretamero@gmail.com>
2023-09-18 20:57:00 -0300

    chore: config static files

--- src/App.ts
+++ src/App.ts
@@ -1,24 +1,26 @@
-import express, { type Express } from 'express';
-import cors from 'cors';
+import express, { type Express } from "express";
+import cors from "cors";
 
-import { errorHandler } from './middlewares/ErrorHandler';
-import { corsOptions } from './middlewares/Cors';
-import { routes } from './Router';
+import { errorHandler } from "./middlewares/ErrorHandler";
+import { corsOptions } from "./middlewares/Cors";
+import { routes } from "./Router";
+import { join } from "path";
 
 class App {
   public readonly app: Express;
 
   constructor() {
     this.app = express();
-    this.app.use(routes);
     this.middlewares();
+    this.app.use(routes);
     this.errorHandler();
   }
 
   private middlewares(): void {
     this.app.use(express.json());
-    this.app.use(cors(corsOptions));
     this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.static(join(__dirname, "public")));
+    this.app.use(cors(corsOptions));
   }
 
   private errorHandler(): void {
