1d1251382e0af417fbade1feab2fd2ec6d0016ea
Alexandre Retamero <alexandreretamero@gmail.com>
2023-09-18 20:57:30 -0300

    feat: execute userRoutes and notesRoutes

--- src/Router.ts
+++ src/Router.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 class Routes {
   public readonly routes = Router();
@@ -7,12 +7,12 @@ class Routes {
     this.userRoutes();
     this.notesRoutes();
     this.helloRoutes();
+    this.userRoutes();
+    this.notesRoutes();
   }
 
   private helloRoutes(): void {
-    this.routes.get('/', (_req, res) =>
-      res.status(200).json({ message: 'Hello world!' }),
-    );
+    this.routes.get("/", (_req, res) => res.json({ message: "Hello world!" }));
   }
 
   private userRoutes(): void {}
