5952e7104265c89faaab570c47c32cebd5874bb0
Alexandre Retamero <alexandreretamero@gmail.com>
2023-10-14 17:24:02 -0300

    fix: remove HOST

--- src/server.ts
+++ src/server.ts
@@ -5,15 +5,14 @@ import { run } from "./database";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocs from "./swagger.json";
 
-const HOST = process.env.HOST ?? "localhost";
 const PORT = Number(process.env.PORT) ?? 3333;
 
 run()
   .then(() => {
     app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-    app.listen(PORT, HOST, () => {
-      console.log(`Server running on http://${HOST}:${PORT} 🔥`);
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT} 🔥`);
     });
   })
   .catch((error) => console.log(error));
