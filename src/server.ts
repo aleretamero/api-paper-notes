80d7687a72d4bc54224a9166e161326bda3b44cb
Alexandre Retamero <alexandreretamero@gmail.com>
2023-10-22 22:14:34 -0300

    feat: add new Environment

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,11 @@ import swaggerUi from "swagger-ui-express";
 import swaggerDocs from "./swagger.json";
 
 const PORT = Number(process.env.PORT) ?? 3333;
+const SWAGGER_URL = process.env.SWAGGER_URL ?? "/api-docs";
 
 run()
   .then(() => {
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+    app.use(SWAGGER_URL, swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT} 🔥`);
